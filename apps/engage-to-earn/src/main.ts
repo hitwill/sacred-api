b86ad7da014f4ce0a89b811d387bcb41e944cf75
skyfoxx <skyfoxx@sacred.finance>
2024-07-07 18:30:13 +1000

    feat: add validation pipe

--- apps/engage-to-earn/src/main.ts
+++ apps/engage-to-earn/src/main.ts
@@ -8,12 +8,16 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
 
+import { ValidationPipe } from '@nestjs/common';
+
 import * as dotenv from 'dotenv';
 dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+  app.useGlobalPipes(new ValidationPipe());
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(
