d2afa3959d59ed9c909e53adb53ecd5d39eb2a5b
skyfoxx <skyfoxx@sacred.finance>
2024-07-07 18:31:05 +1000

    feat: add DTO for votes

--- apps/engage-to-earn/src/app/votes/dto/create-vote.dto.ts
+++ apps/engage-to-earn/src/app/votes/dto/create-vote.dto.ts
@@ -1,1 +1,38 @@
-export class CreateVoteDto {}
+import {
+  IsString,
+  IsNumber,
+  IsEthereumAddress,
+  Min,
+  Max,
+  IsNotEmpty,
+} from 'class-validator';
+
+export class CreateVoteDto {
+  @IsString()
+  @IsNotEmpty()
+  contentHash: string;
+
+  @IsString()
+  @IsNotEmpty()
+  userId: string;
+
+  @IsEthereumAddress()
+  userAddress: string;
+
+  @IsNumber()
+  @Min(-1)
+  @Max(1)
+  vote: number;
+
+  constructor(
+    contentHash: string,
+    userId: string,
+    userAddress: string,
+    vote: number
+  ) {
+    this.contentHash = contentHash;
+    this.userId = userId;
+    this.userAddress = userAddress;
+    this.vote = vote;
+  }
+}
