307339d672ea98ea22b59cf9b2f24b4e417a24ef
skyfoxx <skyfoxx@sacred.finance>
2024-07-07 13:28:24 +1000

    Initial commit

---
+++ apps/engage-to-earn/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'engage-to-earn',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }]
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/engage-to-earn'
+};
