f0f72fdbfcdaf70ce0a71716e58bd0ac831a876f
skyfoxx <skyfoxx@sacred.finance>
2024-07-07 13:36:13 +1000

    feat: add votes/ rewards endpoints

--- apps/engage-to-earn/src/app/app.module.ts
+++ apps/engage-to-earn/src/app/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { VotesModule } from './votes/votes.module';
+import { RewardsModule } from './rewards/rewards.module';
 
 @Module({
-  imports: [],
+  imports: [VotesModule, RewardsModule],
   controllers: [AppController],
   providers: [AppService],
 })
