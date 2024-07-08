186f63e9a7ad3b32ebaecb904ba7fa4493e5df44
skyfoxx <skyfoxx@sacred.finance>
2024-07-08 15:12:27 +1000

    feat: add repositories to votes and rewards

--- apps/engage-to-earn/src/app/votes/votes.service.ts
+++ apps/engage-to-earn/src/app/votes/votes.service.ts
@@ -1,17 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { CreateVoteDto } from './dto/create-vote.dto';
 import { UpdateVoteDto } from './dto/update-vote.dto';
-
-import { Repository, Schema } from 'redis-om';
+import { Schema } from '../schemas';
 import { RedisService } from '../redis/redis.service';
 import { RedisClientType } from 'redis';
+import { Repository } from 'redis-om';
 
 @Injectable()
 export class VotesService {
   private readonly redisClient: RedisClientType;
+  votesRepository: Repository;
 
   constructor(private redisService: RedisService) {
-    this.redisClient = this.redisService.client;
+    this.votesRepository = new Repository(
+      Schema.votes,
+      this.redisService.client
+    );
   }
   create(createVoteDto: CreateVoteDto) {
     return 'This action adds a new vote';
