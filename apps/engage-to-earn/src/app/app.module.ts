76b50797c0133c0616578945e7126dc84ab9a6ac
skyfoxx <skyfoxx@sacred.finance>
2024-07-08 14:25:47 +1000

    refactor: make redis accessible as service

--- apps/engage-to-earn/src/app/app.module.ts
+++ apps/engage-to-earn/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { VotesModule } from './votes/votes.module';
 import { RewardsModule } from './rewards/rewards.module';
+import { RedisModule } from './redis/redis.module';
 
 @Module({
-  imports: [VotesModule, RewardsModule],
+  imports: [VotesModule, RewardsModule, RedisModule],
   controllers: [AppController],
   providers: [AppService],
 })
